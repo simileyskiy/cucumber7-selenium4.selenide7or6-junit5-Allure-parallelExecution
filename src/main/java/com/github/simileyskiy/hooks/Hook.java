d1d2755c008527af9c52531751d6f428eeff14f4
kromp <-@>
2024-01-04 06:34:01 +0300

    add After annotation for close drivers

--- src/main/java/com/github/simileyskiy/hooks/Hook.java
+++ src/main/java/com/github/simileyskiy/hooks/Hook.java
@@ -2,8 +2,11 @@ package com.github.simileyskiy.hooks;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Screenshots;
+import com.codeborne.selenide.WebDriverRunner;
 import io.cucumber.java.*;
 import io.qameta.allure.Allure;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -30,4 +33,16 @@ public class Hook {
             }
         }
     }
+
+    @After
+    public static void tearDown() {
+        try {
+            WebDriver driver = WebDriverRunner.getWebDriver();
+            driver.close();
+            driver.quit();
+        } catch (WebDriverException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
